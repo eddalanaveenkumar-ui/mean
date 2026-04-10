c561a927bc996df3c020890d8e7775a0babfc547
eddalanaveenkumar-ui <eddalanaveenkumar@gmail.com>
2026-04-10 21:36:31 +0530

    chore: integrate vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AppProvider } from './context/AppContext.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </AppProvider>
   </React.StrictMode>,
 )
